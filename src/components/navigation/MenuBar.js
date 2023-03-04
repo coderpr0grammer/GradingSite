7f36f27a0395b2ed108e90a0df8f4e53a04bc08a
kimchiloof <kevin.2005.ge@gmail.com>
2023-03-03 20:57:34 -0500

    Sign in button update

--- src/components/navigation/MenuBar.js
+++ src/components/navigation/MenuBar.js
@@ -18,7 +18,7 @@ function MenuBar() {
         <Nav>
           <Nav.Link style={{textAlign: 'right'}} href="/my-assignments"><span to="/my-assignments" style={{textDecoration: 'none', color: 'white'}}>My Assignments</span></Nav.Link>
           <Nav.Link style={{textAlign: 'right'}} href="/discover"><span to="/discover" style={{textDecoration: 'none', color: 'white'}}>Discover</span></Nav.Link>
-          <Button variant="primary">Sign in</Button>
+          <Button variant="primary" class="sign-in-button"> Sign in </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
