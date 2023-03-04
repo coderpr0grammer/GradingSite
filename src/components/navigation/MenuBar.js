6129a07858097d20bd64a51ff6891e1b2d17dfd6
Daniel Martinez <daniellhf@icloud.com>
2023-03-03 19:15:05 -0500

    added logo in assets folder

--- src/components/navigation/MenuBar.js
+++ src/components/navigation/MenuBar.js
@@ -3,12 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './MenuBar.css';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+
 
 function MenuBar() {
   return (
     <Navbar className="color-nav" collapseOnSelect expand="lg" variant="light" style={{padding: 20}}>
       <Container fluid>
-        <Navbar.Brand><Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>GradingSite</Link></Navbar.Brand>
+        <Navbar.Brand><Link to="/" style={{textDecoration: 'none', color: 'white'}}><img src={Logo} width="50"/>&nbsp;GradingSite</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
