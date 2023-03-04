8e0594e6c4f35bd160a4bf9b5372f744c42fd81b
sophian3105 <sophian3105@gmail.com>
2023-03-03 21:31:48 -0500

    Revert "Update MyAssignments.js"
    This reverts commit 6c30e3624180acf48fd4f8fe863c71739cf5e388.

--- src/components/pages/MyAssignments/MyAssignments.js
+++ src/components/pages/MyAssignments/MyAssignments.js
@@ -1,26 +1,38 @@
 import React from "react";
-import { PieChart, Pie } from "recharts";
+import { PieChart, Pie, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from "recharts";
 import "./MyAssignments.css";
 
 function MyAssignments() {
-  const data = [{ name: "grade", score: 80, fill: "#004A62" }];
+  const donutData = [{ name: "grade", score: 80, fill: "#1B0166" }];
+  const barChart = [{ "name": 'Bar 1', "Amount": 5 }, { "name": 'Bar 2', "Amount": 3 }, { "name": 'Bar 3', "Amount": 9 }]
 
   return (
     <div className="App">
       <header className="App-header">
       <h1>Grade Your Papers Now</h1>
-        <PieChart width={700} height={700}>
-        <Pie data={data} dataKey="score" outerRadius={250} fill="#ECF8E5" innerRadius={150} startAngle={90} endAngle={450}/>
+      var essay = prompt("Essay", "Insert your text here, upload a file, or scan a paper to start the evaluation...");
+        <PieChart width={400} height={400}>
+        <Pie data={donutData} dataKey="score" outerRadius={170} fill="#1B0166" innerRadius={100} startAngle={90} endAngle={90 + donutData[0].score*3.6}/>
         </PieChart>
-      </header>
-      <body>
-        <h3>Create Input Box</h3>
-        <input type="text" id="myName" placeholder="Enter Name">
-        <button id="btn">Save</button>
-        <script src="code.js"></script>
-      </body>
+        <p id="gradeLabel" >{donutData[0].score}%</p>
+      
+        
+
+
+
+  <BarChart width={300} height={200} data={barChart}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  {/* <YAxis /> */}
+  <Tooltip />
+  <Legend />
+  <Bar dataKey="Amount" fill="#8884d8" />
+</BarChart>
+
+
+
+    </header>
     </div>
-    
   );
 }
 
