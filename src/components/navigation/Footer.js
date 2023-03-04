929d8db30df3b6128e4b8c5cee5b9a55c7aedd0d
EdwinNgui <edwin.nguim@gmail.com>
2023-03-03 19:42:57 -0500

    Revert "Testing footer"
    This reverts commit bf85c8e5f7688c2fc57eaa9a22f0e4c835d337d8.

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="footer">
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/my-assignments">Information</Link></li>
+                    <li><Link to="/my-assignments">Home</Link></li>
                     <li><Link to="#">Contact Us</Link></li>
                 </ul>
 
