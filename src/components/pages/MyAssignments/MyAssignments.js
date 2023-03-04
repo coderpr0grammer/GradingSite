cd765c7cc10e9e5a1f9bd93c9b412a8c309fb76e
EdwinNgui <edwin.nguim@gmail.com>
2023-03-03 20:29:23 -0500

    Animated circle

--- src/components/pages/MyAssignments/MyAssignments.js
+++ src/components/pages/MyAssignments/MyAssignments.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { PieChart, Pie } from "recharts";
+import "./MyAssignments.css";
 
 function MyAssignments() {
-  const data = [{ name: "homeUtil", Grade: 90, fill: "#004A62" }];
+  const data = [{ name: "grade", score: 80, fill: "#004A62" }];
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>this is a test right ehre</h1>;
         <PieChart width={700} height={700}>
-          <Pie
-            data={data}
-            dataKey="grade"
-            outerRadius={250}
-            fill="#ECF8E5"
-            innerRadius={150}
-            startAngle={90}
-            endAngle={450}
-          />
+        <Pie data={data} dataKey="score" outerRadius={250} fill="#ECF8E5" innerRadius={150} startAngle={90} endAngle={450}/>
         </PieChart>
       </header>
     </div>
   );
 }
+
+
+
 
 export default MyAssignments;
