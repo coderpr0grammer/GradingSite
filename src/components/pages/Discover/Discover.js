0a378292f2ae3093c725eb1893a65924b8d2b425
kimchiloof <kevin.2005.ge@gmail.com>
2023-03-04 10:09:38 -0500

    Discover.js code format

--- src/components/pages/Discover/Discover.js
+++ src/components/pages/Discover/Discover.js
@@ -9,90 +9,75 @@ function Discover() {
     <div className="App">
       <header className="App-header">
       
-      
-      
-      <div class="row" id="DiscoverHeader">
-    <div class="col-6" >
-    <h1 id="DiscoverHeaderText">Discover what other professors, teachers, and alike are up to</h1>
-    </div>
-    <div class="col">
-      {/* //ISSUE: should have img plus icon here */}
-    <h1 id="DiscoverImg">picture goes here</h1>
-    </div>
-  </div>
+        <div class="row" id="DiscoverHeader">
+          <div class="col-6" >
+            <h1 id="DiscoverHeaderText">Discover what other professors, teachers, and alike are up to</h1>
+          </div>
+          <div class="col">
+            {/* //ISSUE: should have img plus icon here */}
+            <h1 id="DiscoverImg">picture goes here</h1>
+          </div>
+        </div>
   
-  <div class="row">
-    <div class="col">
-    <form class="searchSort">
-  <label for="gsearch"></label>
-  <input type="search" placeholder="Search for an Assignment..." id="gsearch"/>
-</form>
-    </div>
-    <div class="col">
+        <div class="row">
+          <div class="col">
+            <form class="searchSort">
+              <label for="gsearch"></label>
+              <input type="search" placeholder="Search for an Assignment..." id="gsearch"/>
+            </form>
+          </div>
+          <div class="col">
+            <select class="categoryDropDown">
+              <option class="categoryDropDownChoice">Sort A-Z</option>
+              <option class="categoryDropDownChoice">Sort Z-A</option>
+            </select>
+          </div>
+        </div>
+          
+        <div class="row">
+          <div class="col">
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title class="cardTitle">Understanding the World Through Marxist Lens</Card.Title>
+                <Card.Text class="cardDesc">By: Carlene Adams</Card.Text>
+                <Card.Text class="cardDesc">
+                  This assignment explores the use of marxist theory and lens to analyze the inner workings of literacture that has been used to impact the world in a positive manner. This has been inspired by the...
+                </Card.Text>
+                <a href="/grade"><Button variant="primary">Use Assignment</Button></a>
+              </Card.Body>
+            </Card>
+          </div>
+          <div class="col">
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title class="cardTitle">How Has the World  Been Influenced by the novel 1984?</Card.Title>
+                <Card.Text class="cardDesc">By: Jackson James</Card.Text>
+                <Card.Text class="cardDesc">
+                  This assignment explores the use of authoritarian rule to analyze the inner workings of literature that has been used to impact the world in a positive manner. This has been inspired by the...
+                </Card.Text>
+                <a href="/grade"><Button variant="primary">Use Assignment</Button></a>
+              </Card.Body>
+            </Card>
+          </div>
+          <div class="col">
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title class="cardTitle">The Fundamental Philosophical Natures</Card.Title>
+                <Card.Text class="cardDesc">By: Maxine Deans</Card.Text>
+                <Card.Text class="cardDesc">
+                  This assignment explores the use of authoritarian rule to analyze the inner workings of literature that has been used to impact the world in a positive manner. This has been inspired by the...
+                </Card.Text>
+                <a href="/grade"><Button variant="primary">Use Assignment</Button></a>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
 
-    <select class="categoryDropDown">
-<option class="categoryDropDownChoice">Sort A-Z</option>
-<option class="categoryDropDownChoice">Sort Z-A</option>
-</select>
-    </div>
-  </div>
-    
-  <div class="row">
-    <div class="col">
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title class="cardTitle">Understanding the World Through Marxist Lens</Card.Title>
-        <Card.Text class="cardDesc">
-        By: Carlene Adams
-        </Card.Text>
-        <Card.Text class="cardDesc">
-        This assignment explores the use of marxist theory and lens to analyze the inner workings of literacture that has been used to impact the world in a positive manner. This has been inspired by the...
-        </Card.Text>
-        <a href="/grade">
-        <Button variant="primary">Use Assignment</Button>
-      </a>
-      </Card.Body>
-    </Card>
-    </div>
-    <div class="col">
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-        <Card.Title class="cardTitle">How Has the World  Been Influenced by the novel 1984?</Card.Title>
-        <Card.Text class="cardDesc">
-        By: Jackson James
-        </Card.Text>
-        <Card.Text class="cardDesc">
-        This assignment explores the use of authoritarian rule to analyze the inner workings of literature that has been used to impact the world in a positive manner. This has been inspired by the...
-        </Card.Text>
-        <a href="/grade">
-        <Button variant="primary">Use Assignment</Button>
-      </a>
-      </Card.Body>
-    </Card>
-    </div>
-    <div class="col">
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-        <Card.Title class="cardTitle">The Fundamental Philosophical Natures</Card.Title>
-        <Card.Text class="cardDesc">
-        By: Maxine Deans
-        </Card.Text>
-        <Card.Text class="cardDesc">
-        This assignment explores the use of authoritarian rule to analyze the inner workings of literature that has been used to impact the world in a positive manner. This has been inspired by the...
-        </Card.Text>
-        <a href="/grade">
-        <Button variant="primary">Use Assignment</Button>
-      </a>
-      </Card.Body>
-    </Card>
-    </div>
-  </div>
+        <div id="inspirationContainer">
+            <h1 id="inspirationBtn">Find More Inspiration</h1>
+        </div>
 
-<div id="inspirationContainer">
-    <h1 id="inspirationBtn">Find More Inspiration</h1>
-</div>
-
-    </header>
+      </header>
     </div>
   );
 }
