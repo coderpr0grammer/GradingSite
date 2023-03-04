b25515932d7b6b1d1409daf23cdc510902dbe190
Daniel Martinez <daniellhf@icloud.com>
2023-03-03 20:13:49 -0500

    fixed myassignments

--- src/components/pages/MyAssignments/MyAssignments.js
+++ src/components/pages/MyAssignments/MyAssignments.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import {PieChart, Pie} from 'recharts';
+import React from "react";
+import { PieChart, Pie } from "recharts";
 
-<h1>this is a test right ehre</h1>
-
-function App() {
-
-  const data = [
-    {name: 'homeUtil', Grade: 90, fill: '#004A62'}
-  ];
+function MyAssignments() {
+  const data = [{ name: "homeUtil", Grade: 90, fill: "#004A62" }];
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>this is a test right ehre</h1>;
         <PieChart width={700} height={700}>
-          <Pie data={data} dataKey="grade" outerRadius={250} fill="#ECF8E5" innerRadius={150} startAngle={90} endAngle={450}/>
+          <Pie
+            data={data}
+            dataKey="grade"
+            outerRadius={250}
+            fill="#ECF8E5"
+            innerRadius={150}
+            startAngle={90}
+            endAngle={450}
+          />
         </PieChart>
       </header>
     </div>
   );
 }
 
-export default App;
+export default MyAssignments;
